582c45576fe41eb83e22fbacbcadd98bb796c0d3
Akevonwolfe <94.andreas.svensson@gmail.com>
2025-06-04 21:35:50 +0200

    test for addbook

---
+++ src/tests/addbook.spec.js
@@ -0,0 +1,98 @@
+import {test, expect} from '@playwright/test'
+
+const book = {title: "Red Sister", author: "Mark Lawrence"}
+
+test.describe("Add book", () => {
+    test.beforeEach(async ({page}) => {
+         await page.goto("https://tap-ht24-testverktyg.github.io/exam-template/")
+    })
+
+    test("add book to catalog", async ({page}) => {
+        await page
+              .getByTestId("add-book")
+              .click()
+        await expect(page
+              .getByTestId("add-book"))
+              .toBeDisabled()
+        await page
+              .getByTestId("add-input-title")
+              .click()
+        await page
+              .getByTestId("add-input-title")
+              .fill(book.title)
+        await page
+              .getByTestId("add-input-author")
+              .click()
+        await page
+              .getByTestId("add-input-author")
+              .fill(book.author)
+        await page
+              .getByTestId("add-submit")
+              .click()
+    })
+    test("Check if book is added", async ({page}) => {
+        await page
+              .getByTestId("add-book")
+              .click()
+        await expect(page
+              .getByTestId("add-book"))
+              .toBeDisabled()
+        await page
+              .getByTestId("add-input-title")
+              .click()
+        await page
+              .getByTestId("add-input-title")
+              .fill(book.title)
+        await page
+              .getByTestId("add-input-author")
+              .click()
+        await page
+              .getByTestId("add-input-author")
+              .fill(book.author)
+        await page
+              .getByTestId("add-submit")
+              .click()
+        await page
+              .getByTestId("catalog")
+              .click()
+        await expect(page
+              .getByText(book.title, book.author))
+              .toBeVisible()
+    })
+
+    test("Check if book is not added with empty title or author", async ({page}) => {
+        await page
+              .getByTestId("add-book")
+              .click()
+        await expect(page
+              .getByTestId("add-book"))
+              .toBeDisabled()
+        await expect(page
+              .getByTestId("add-submit"))   
+              .toBeDisabled()                // Check if submit is disabled
+        await page
+              .getByTestId("add-input-author")
+              .click()
+        await page
+              .getByTestId("add-input-author")
+              .fill(book.author)
+        await expect(page
+              .getByTestId("add-submit"))
+              .toBeDisabled()               // Check if submit is still disabled with author only
+        await page
+              .getByTestId("add-input-title")
+              .click()
+        await page
+              .getByTestId("add-input-title")
+              .fill(book.title)
+        await page
+              .getByTestId("add-input-author")
+              .click()
+        await page
+              .getByTestId("add-input-author")
+              .fill("")                       // Clear author field
+        await expect(page
+              .getByTestId("add-submit"))
+              .toBeDisabled()               // Check if submit is still disabled with title only
+    })
+})
