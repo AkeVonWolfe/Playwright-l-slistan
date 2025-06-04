69c824e06df39da21a9eb1e184cb576ec72679d9
Akevonwolfe <94.andreas.svensson@gmail.com>
2025-06-04 21:03:05 +0200

    test for catalog

---
+++ src/tests/catalog.spec.js
@@ -0,0 +1,35 @@
+import {test, expect} from '@playwright/test'
+
+test.describe("Catalog", () => {
+    test.beforeEach(async ({page}) => {
+        await page.goto("https://tap-ht24-testverktyg.github.io/exam-template/")
+    })
+
+    test("add book to favorites", async ({page}) => {
+        await page
+              .getByTestId("star-Hur man tappar bort sin TV-fjärr 10 gånger om dagen")
+              .click()
+        await expect(page
+              .getByTestId("star-Hur man tappar bort sin TV-fjärr 10 gånger om dagen"))
+              .toBeVisible()
+        
+    })
+
+    test ("remove book from favorites", async ({page}) => {
+        await page
+              .getByTestId("star-Hur man tappar bort sin TV-fjärr 10 gånger om dagen")
+              .click()
+        await expect(page
+              .getByTestId("star-Hur man tappar bort sin TV-fjärr 10 gånger om dagen"))
+              .toBeVisible()
+        await page
+              .getByTestId("star-Hur man tappar bort sin TV-fjärr 10 gånger om dagen")
+              .click()
+        await expect(page
+              .getByTestId("star-Hur man tappar bort sin TV-fjärr 10 gånger om dagen"))
+              .not.toHaveClass(/star.selected/) // hur skulle .not.toHaveCSS() med opacity ?
+              
+    })
+
+
+})
